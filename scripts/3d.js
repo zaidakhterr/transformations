5db0e04be40d2d1d1f71de9f38bf9bf611baed29
Sarahmdawood <51886388+Sarahmdawood@users.noreply.github.com>
2019-12-29 16:53:26 +0500

    Translation worked

--- scripts/3d.js
+++ scripts/3d.js
@@ -2,8 +2,9 @@ const display = () => {
   let x = document.getElementById('translate-x').value;
   let y = document.getElementById('translate-y').value;
   let div = document.getElementById('translate-div');
+  console.log(x);
 
-  div.style.transform = `translateZ(${x})';
+  div.style.transform = `translate(${x})`;
 
   console.log(x);
   console.log(y);
