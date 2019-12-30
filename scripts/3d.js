b0daf02f52e47f9807c5742c7e824f5343b3fb4b
Sarahmdawood <51886388+Sarahmdawood@users.noreply.github.com>
2019-12-30 14:53:38 +0500

    Translation and rotate fixed

--- scripts/3d.js
+++ scripts/3d.js
@@ -15,47 +15,50 @@ const display = () => {
   let y = document.getElementById('translate-y').value;
   let z = document.getElementById('translate-z').value;
   let div = document.getElementById('translate-div');
-  console.log(x);
-  console.log(y);
-  console.log(z);
+  //   console.log(x);
+  //   console.log(y);
+  //   console.log(z);
   if (x && y && z) {
     div.style.transform = `perspective(800px) translate3d(${x},${y},${z})`;
   } else if (x && y) {
     div.style.transform = `translateX(${x}) translateY(${y})`;
+  } else if (x && z) {
+    div.style.transform = `perspective(800px) translateX(${x}) translateZ(${z}`;
+  } else if (y && z) {
+    div.style.transform = `perspective(800px) translateY(${y}) translateZ(${z}`;
   } else if (x) {
     div.style.transform = `translateX(${x})`;
   } else if (y) {
     div.style.transform = `translateY(${y})`;
   } else if (z) {
     div.style.transform = `perspective(800px) translateZ(${z})`;
-  } else if (x && z) {
-    div.style.transform = `perspective(800px) translateX(${x}) translateZ(${z})`;
-  } else if (y && z) {
-    div.style.transform = `perspective(800px) translateY(${y}) translateZ(${z})`;
   }
 };
 
 const rotate = () => {
   let angle = document.getElementById('angle').value;
   let div = document.getElementById('rotate-div');
-  let checkx = document.getElementById('x-axis');
-  let checky = document.getElementById('y-axis');
-  let checkz = document.getElementById('z-axis');
+  let x = document.getElementById('x-axis').value;
+  let y = document.getElementById('y-axis').value;
+  let z = document.getElementById('z-axis').value;
   //   console.log(angle);
-  if (checkx.checked && checky.checked && checkz.checked) {
-    div.style.transform = `rotate3d(1,1,1,${angle})`;
-  } else if (checkx.checked && checky.checked) {
-    div.style.transform = `rotate3d(1,1,0,${angle})`;
-  } else if (checkx.checked && checkz.checked) {
-    div.style.transform = `rotate3d(1,0,1,${angle})`;
-  } else if (checkx.checked) {
-    div.style.transform = `rotate3d(1,0,0,${angle})`;
-  } else if (checky.checked && checkz.checked) {
-    div.style.transform = `rotate3d(0,1,1,${angle})`;
-  } else if (checky.checked) {
-    div.style.transform = `rotate3d(0,1,0,${angle})`;
-  } else if (checkz.checked) {
-    div.style.transform = `rotate3d(0,0,1,${angle})`;
+  if (x && y && z) {
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    div.style.transform = `rotate3d(${x},${y},${z},${angle})`;
+  } else if (x && y) {
+    div.style.transform = `rotate3d(${x},${y},0,${angle})`;
+  } else if (x && z) {
+    div.style.transform = `rotate3d(${x},0,${z},${angle})`;
+  } else if (y && z) {
+    div.style.transform = `rotate3d(0,${y},${z},${angle})`;
+  } else if (x) {
+    div.style.transform = `rotate3d(${x},0,0,${angle})`;
+  } else if (y) {
+    div.style.transform = `rotate3d(0,${y},0,${angle})`;
+  } else if (z) {
+    div.style.transform = `rotate3d(0,0,${z},${angle})`;
   }
 };
 
@@ -68,18 +71,20 @@ const scale = () => {
   console.log(y);
   console.log(z);
   if (x && y && z) {
-    div.style.transform = `perspective(800px) scale3d(${x},${y},${z})`;
+    //temporary
+    div.style.perspectiveOrigin = 'top';
+    div.style.transform = `perspective(300px) scale3d(${x},${y},${z})`;
   } else if (x && y) {
     div.style.transform = `scaleX(${x}) scaleY(${y})`;
+  } else if (x && z) {
+    div.style.transform = `perspective(800px) scaleX(${x}) scaleZ(${z})`;
+  } else if (y && z) {
+    div.style.transform = `perspective(800px) scaleY(${y}) scaleZ(${z})`;
   } else if (x) {
     div.style.transform = `scaleX(${x})`;
   } else if (y) {
     div.style.transform = `scaleY(${y})`;
   } else if (z) {
     div.style.transform = `perspective(800px) scaleZ(${z})`;
-  } else if (x && z) {
-    div.style.transform = `perspective(800px) scaleX(${x}) scaleZ(${z})`;
-  } else if (y && z) {
-    div.style.transform = `perspective(800px) scaleY(${y}) scaleZ(${z})`;
   }
 };
